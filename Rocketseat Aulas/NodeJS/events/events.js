584254bf1ce283874a2aa7559fb66251927d26dc
Ícaro Batista <73768646+ibaptiista@users.noreply.github.com>
2023-03-22 00:58:10 -0300

    Add files via upload

---
+++ Rocketseat Aulas/NodeJS/events/events.js
@@ -0,0 +1,26 @@
+// const { EventEmitter } = require('events')
+
+// const ev = new EventEmitter()
+
+// ev.once('saySomething', (message) => {
+//     console.log('Eu ouvi você: ', message);
+// })
+
+// ev.emit('saySomething', "Mayk")
+// ev.emit('saySomething', "João")
+// ev.emit('saySomething', "Maria")
+
+const { inherits } = require('util')
+const { EventEmitter } = require('events')
+
+function Character(name) {
+    this.name = name
+}
+
+inherits(Character, EventEmitter)
+
+const chapolin = new Character('Chapolin')
+chapolin.on('help', () => console.log(`Eu! o ${chapolin.name} colorado!`))
+
+console.log('Oh! E agora, quem poderá me defender?')
+chapolin.emit('help')
